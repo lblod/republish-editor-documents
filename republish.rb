787221556006d145df435988107027b103c5b873
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-01-16 11:23:19 +0100

    updated script

--- republish.rb
+++ republish.rb
@@ -10,6 +10,7 @@ require 'tempfile'
 require 'csv'
 require 'pry-byebug'
 require 'digest/sha1'
+require 'httpclient'
 
 class Republisher
   attr_reader :client, :log
@@ -34,24 +35,33 @@ class Republisher
                 "agenda publiek" => "http://mu.semte.ch/application/editor-document-statuses/627aec5d144c422bbd1077022c9b45d1",
                 "goedgekeurd" => "http://mu.semte.ch/application/editor-document-statuses/c272d47d756d4aeaa0be72081f1389c6"}
 
-  def initialize(endpoint)
+  def initialize(endpoint, publish_endpoint)
     @endpoint = endpoint
-    @client = SPARQL::Client.new(endpoint)
+    @publish_endpoint = publish_endpoint
+    options = {}
+    options[:read_timeout] = 360
+    @client = SPARQL::Client.new(endpoint, options)
     @log = Logger.new(STDOUT)
     @log.level = Logger::INFO
     wait_for_db
     @manual_check = []
     @published_status_no_publication = []
+    @failed_deletes = []
+    @failed_publish = []
+    @ok_publish = []
   end
 
   def run()
     docs_info = find_docs_to_republish()
-    binding.pry
-    cleanup_published_page(docs_info)
+    docs_info.each do |triple|
+      has_delete_errors = cleanup_published_page(triple)
+      if has_delete_errors
+        next
+      end
+      republish(triple)
+    end
 
-    # proceed to cleanup
-
-    print_things_to_check_manually
+    write_log
   end
 
   def find_docs_to_republish()
@@ -73,26 +83,83 @@ class Republisher
     docs_info
   end
 
-  def cleanup_published_page(docs_info)
-    docs_info.each do |triple|
-      triples = find_zittingen_linked_to_eenheid(triple.eenheid.value)
-      p "Found #{triples.length} docs"
-      if(triples.length == 0)
-        p "No zitting found for #{triple.eenheidNaam.value}, #{triple.eenheidType.value}"
-        @published_status_no_publication << triple
-        next
-      end
+  def cleanup_published_page(triple)
+    has_errors = false
+    triples = find_zittingen_linked_to_eenheid(triple.eenheid.value)
+    p "Found #{triples.length} docs"
+    if(triples.length == 0)
+      p "No zitting found for #{triple.eenheidNaam.value}, #{triple.eenheidType.value}"
+      @published_status_no_publication << triple
+      return
+    end
 
-      triples.each do |zitting|
-        cleanup_zitting(zitting)
-        p "Cleaned #{zitting.eenheidNaam.value}, #{zitting.eenheidType.value} #{zitting.zitting.value}"
+    triples.each do |zitting|
+      begin
+       cleanup_zitting(zitting)
+       p "Cleaned #{zitting.eenheidNaam.value}, #{zitting.eenheidType.value} #{zitting.zitting.value}"
+      rescue
+        @failed_deletes << zitting
+        has_errors = true
       end
     end
+    has_errors
   end
 
   def cleanup_zitting(triple)
     remove_zitting_with_besluiten(triple.zittingId.value)
     remove_zitting_with_agenda_only(triple.zittingId.value)
+  end
+
+  def republish(triple)
+    p "Starting publish"
+    endpoint = @publish_endpoint
+    agenda = endpoint + "/publish/agenda/#{triple.docId.value}"
+    besluiten = endpoint + "/publish/decision/#{triple.docId.value}"
+    notulen = endpoint + "/publish/notule/#{triple.docId.value}"
+
+    inverted_mapping = DOCSTATES.invert
+
+    begin
+      if inverted_mapping[triple.status.value] == "goedgekeurd"
+        post(agenda)
+        post(besluiten)
+        post(notulen)
+        p " publishing ok for EENHEID: #{triple.eenheidNaam.value}, TYPE #{triple.eenheidType.value}"
+        @ok_publish << triple
+        return
+      end
+
+      if  inverted_mapping[triple.status.value] == "besluitenlijst publiek"
+        post(agenda)
+        post(besluiten)
+        p " publishing ok for EENHEID: #{triple.eenheidNaam.value}, TYPE #{triple.eenheidType.value}"
+        @ok_publish << triple
+        return
+      end
+
+      if  inverted_mapping[triple[-1].status.value] == "agenda publiek"
+        post(agenda)
+         p " publishing ok for EENHEID: #{triple.eenheidNaam.value}, TYPE #{triple.eenheidType.value}"
+        @ok_publish << triple
+        return
+      end
+    rescue
+      p " publishing failed for EENHEID: #{triple.eenheidNaam.value}, TYPE #{triple.eenheidType.value}"
+      @failed_publish << triple
+      has_error = true
+    end
+
+  end
+
+  def post(endpoint)
+    client = HTTPClient.new
+    method = 'POST'
+    url = URI.parse endpoint
+    res = client.request method, url
+    if not 200 >= res.status_code and not res.status_code < 300
+      binding.pry
+      raise "Error publishing #{endpoint}"
+    end
   end
 
   def find_graphs_with_doc()
@@ -146,7 +213,7 @@ class Republisher
     PREFIX eli: <http://data.europa.eu/eli/ontology#>
 
     DELETE {
-     GRAPH ?g {
+     GRAPH <http://mu.semte.ch/graphs/public> {
        ?s ?p ?o.
        ?agenda ?agendaP ?agendaO.
        ?agendapunt ?agendapuntP ?agendapuntO.
@@ -172,7 +239,7 @@ class Republisher
       }
     }
   )
-    query(query_str)
+    res = query(query_str)
   end
 
   def remove_zitting_with_agenda_only(zittingUid)
@@ -185,7 +252,7 @@ class Republisher
     PREFIX eli: <http://data.europa.eu/eli/ontology#>
 
     DELETE {
-     GRAPH ?g {
+     GRAPH <http://mu.semte.ch/graphs/public> {
        ?s ?p ?o.
        ?agenda ?agendaP ?agendaO.
        ?agendapunt ?agendapuntP ?agendapuntO.
@@ -202,7 +269,7 @@ class Republisher
       }
     }
   )
-    query(query_str)
+   res = query(query_str)
   end
 
   def find_published_docs(eenheid_g)
@@ -215,10 +282,11 @@ class Republisher
             PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
             PREFIX dct: <http://purl.org/dc/terms/>
 
-            SELECT DISTINCT ?doc ?modified ?eenheidType ?eenheidNaam ?statusName ?status ?content ?title ?eenheid
+            SELECT DISTINCT ?doc ?docId ?modified ?eenheidType ?eenheidNaam ?statusName ?status ?content ?title ?eenheid ?eenheidId
             WHERE {
               GRAPH <http://mu.semte.ch/graphs/public> {
                 ?eenheid mu:uuid "#{uuid}".
+                ?eenheid mu:uuid ?eenheidId.
                 ?eenheid skos:prefLabel ?eenheidNaam.
                 ?eenheid besluit:classificatie ?classS.
                 ?classS skos:prefLabel ?eenheidType.
@@ -231,6 +299,7 @@ class Republisher
                 ?doc ext:editorDocumentStatus ?status.
                 ?doc pav:lastUpdateOn ?modified.
                 ?doc dct:title ?title.
+                ?doc mu:uuid ?docId.
                 ?doc ext:editorDocumentContent ?content.
                 FILTER(
                   NOT EXISTS {
@@ -303,15 +372,39 @@ class Republisher
     (triples.uniq{ |t| t.title.value }).length == 1
   end
 
-  def print_things_to_check_manually
-    p "!!!!!!! Some weird states to check (order or title is not ok):"
-    @manual_check.each do |t|
-      p "- EENHEID: #{t.eenheidNaam.value}, TYPE #{t.eenheidType.value}"
-    end
-    p "!!!!!!! Published in notule but didn't hit besluiten"
-    @published_status_no_publication.each do |t|
-      p "- EENHEID: #{t.eenheidNaam.value}, TYPE #{t.eenheidType.value}"
-    end
+  def write_log
+    file_path = File.join(ENV['OUTPUT_PATH'],"#{DateTime.now.strftime("%Y%m%d%H%M%S")}-publish.log")
+    open(file_path, 'w') { |f|
+      f << "!!!!!!! Some weird states to check (order or title is not ok):"
+
+      @manual_check.each do |t|
+        f << "- EENHEID: #{t.eenheidNaam.value}, TYPE #{t.eenheidType.value}"
+      end
+
+      f <<  "!!!!!!! Published in notule but didn't hit besluiten"
+      @published_status_no_publication.each do |t|
+       f << "- EENHEID: #{t.eenheidNaam.value}, TYPE #{t.eenheidType.value}"
+      end
+
+      f << "!!!!!!! Failed remove publication for zitting:"
+      @failed_deletes.each do |t|
+       f  << "- EENHEID: #{t.eenheidNaam.value}, TYPE #{t.eenheidType.value}"
+      end
+
+      f << "!!!!!!! Failed doc publish for:"
+      @failed_publish.each do |t|
+       f  << "- EENHEID: #{t.eenheidNaam.value}, TYPE #{t.eenheidType.value}"
+      end
+
+      f << "List of all failed publish and delete eenheden"
+      f << (@failed_delete + @failed_publish).reduce([]){ |acc, t| acc << "#{t.eenhedId.value}" }
+
+      f << "list of eenheden ok publish"
+      @ok_publish.each do |t|
+       f  << "- EENHEID: #{t.eenheidNaam.value}, TYPE #{t.eenheidType.value}"
+      end
+
+    }
 
   end
 
@@ -342,5 +435,5 @@ class Republisher
 end
 
 
-mdb = Republisher.new(ENV['ENDPOINT'])
+mdb = Republisher.new(ENV['ENDPOINT'], ENV['PUBLISHENDPOINT'])
 mdb.run()
